ae59ee10f533deedd24e6d613f0f6d72a4f04274
Andrew Nyhus <andrew@yourstake.org>
2023-05-05 12:04:29 -0400

    fix bug

--- ys-embed-sdk/src/your-stake-embed.ts
+++ ys-embed-sdk/src/your-stake-embed.ts
@@ -1,5 +1,16 @@
 import getUrlForIFramePage from "./iframe-routes";
 
+interface YourStakeEmbedArgs {
+    clientKey: string
+    authCode: string
+    targetElementIdentifier: string
+    initialPage: string
+    width: string
+    height: string
+    onLoad: Function | null
+    onError: Function | null
+}
+
 class YourStakeEmbed {
     clientKey: string;
     authCode: string;
@@ -7,26 +18,29 @@ class YourStakeEmbed {
     iframeElement: HTMLIFrameElement;
     currentPage: string;
 
-    constructor(
-        clientKey: string,
-        authCode: string,
-        targetElementIdentifier: string,
-        initialPage: string = 'report-builder',
-        // onLoad: Function | null,
-        // onError: Function | null,
-
-        ) {
+    constructor({
+        clientKey,
+        authCode,
+        targetElementIdentifier,
+        initialPage='report-builder',
+        width,
+        height,
+        // onLoad=null,
+        // onError=null,
+    }: YourStakeEmbedArgs) {
         // todo: auth
         this.clientKey = clientKey;
         this.authCode = authCode;
 
         this.currentPage = initialPage;
-
         this.iframeElement = document.createElement('iframe');
         this.iframeElement.id = 'yourstake-embedded-iframe';
         this.iframeElement.src = getUrlForIFramePage(this.currentPage);
+        this.iframeElement.style.width = width;
+        this.iframeElement.style.height = height;
+
         this.targetElement = document.getElementById(targetElementIdentifier) as HTMLElement;
-        this.targetElement.appendChild(this.iframeElement);        
+        this.targetElement.appendChild(this.iframeElement);
 
     }
 }
