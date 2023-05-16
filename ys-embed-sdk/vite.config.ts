fc36cec0a06737c57eca643f2c6ba9dc46ef39d9
Andrew Nyhus <andrew@yourstake.org>
2023-05-16 12:50:22 -0400

    consolidate build scripts, env specific iframe domain, and plugin for copying the bundle locally

--- ys-embed-sdk/vite.config.ts
+++ ys-embed-sdk/vite.config.ts
@@ -1,26 +1,54 @@
-import { resolve } from 'path'
+import fs from 'fs'
 import { defineConfig } from 'vite'
-let _ = defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/main.ts'),
-            name: 'YourStakeEmbed',
-            fileName: 'yourstake-embed-sdk-v1',
-            formats: ['es'],
+
+let _ = defineConfig(({mode}) => {
+    const iframeDomain = {
+        'production': 'https://www.yourstake.org',
+        'development': 'https://localhost:8443',
+    }[mode];
+
+    return {
+        build: {
+            rollupOptions: {
+                input: 'src/main.ts',
+                output: {
+                    dir: 'dist',
+                    inlineDynamicImports: true,
+                    entryFileNames: 'yourstake-embed-sdk.js',
+                    format: 'es',
+                    sourcemap: 'inline',
+                    name: 'YourStakeEmbed',
+                },
+            }
         },
-        // rollupOptions: {
-        //     input: 'src/main.ts',
-        //     output: {file: 'dist/hi.js', format: 'es', sourcemap: "inline"},
-        //     // output: {
-        //     //     file: 'yourstake-embed-sdk-v1.js',
-        //     //     format: "es",
-        //     //     name: 'YourStakeEmbed',
-        //     // }
-        // }
-    },
-    server: {
-        port: 7173,
-    }
+        plugins: [
+            {
+                name: 'copy-bundle-to-mock-embed-app-when-local',
+                closeBundle: async () => {
+                    if (mode === 'development') {
+                        fs.copyFile(
+                            'dist/yourstake-embed-sdk.js',
+                            '../../embed-mock-app/frontend/src/assets/yourstake-embed-sdk.js',
+                            (error) => {
+                                if (error) {
+                                    console.error('Error while copying bundle to mock embed app: ', error);
+                                    throw error;
+                                } else {
+                                    console.log('Copied bundle file to mock embed app');
+                                }
+                            }
+                        );
+                    }
+                },
+            }
+        ],
+        server: {
+            port: 7173,
+        },
+        define: {
+            __YS_IFRAME_DOMAIN__: iframeDomain,
+        }
+    };
 });
 // console.log("_: ", _.build.rollupOptions.output);
 
